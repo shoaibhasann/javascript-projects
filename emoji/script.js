93508cbdabbb308435bcea3b0122dda4e004bea0
Mohd Shoaib <112479144+shoaibhasann@users.noreply.github.com>
2024-01-23 12:32:40 +0000

    emoji project done

---
+++ emoji/script.js
@@ -0,0 +1,47 @@
+const emojiContainer = document.querySelector('#emoji');
+const emojis = [
+  '😆',
+  '😅',
+  '🤣',
+  '😂',
+  '😀',
+  '🤑',
+  '🤨',
+  '🙂',
+  '😊',
+  '😗',
+  '😛',
+  '😏',
+  '🤥',
+  '😴',
+  '🥺',
+  '😧',
+  '😇',
+  '😳',
+  '🙃',
+  '🥴',
+  '🧐',
+  '🤨',
+  '😒',
+  '🤔',
+  '🤭',
+  '🥰',
+  '🤐',
+  '👀',
+  '🤔',
+  '🤪',
+  '🥲',
+  '😃',
+  '😁',
+  '😬',
+];
+
+let index = 0;
+
+emojiContainer.onmouseover = function(){
+    emojiContainer.innerText = emojis[index];
+    index++;
+    if(index + 1 > emojis.length){
+        index = 0;
+    }
+}
